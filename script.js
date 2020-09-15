8e26970c68800ea376e7273b7b20eb291b11866c
Cody Bonsma <codybonsma@gmail.com>
2020-09-14 21:08:57 -0400

    pulled the temp (set it to F), the humidity and windspeed and set them to variables

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   console.log("Hello world");
 
+  // variables from the html page elements
   var searchInputEl = $("#search-input");
   var searchBtn = $("#search-btn");
   var mainForecastEl = $("#main-forecast");
@@ -9,6 +10,7 @@ $(document).ready(function () {
  // weather app API KEY: cc00bc76b8b458cb3c1c74e38a95cf97 
  // API call by city name: api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
+ // on click, we want to dynamically populate the main forecast and five day forecast 
  searchBtn.on("click", function(){
  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputEl.val() + "&appid=cc00bc76b8b458cb3c1c74e38a95cf97"
 
@@ -16,17 +18,18 @@ $(document).ready(function () {
   url: queryURL,
   method: "GET",
 }).then(function (response) {
-  console.log(response);
+  console.log(response.main.temp);
+  // get the Kelvin temperature from the api and set it to Fahrenheit 
+  var kelvinTemp = response.main.temp;
+  var newTemp = Math.round((( kelvinTemp - 273.15) * 9/5) + 32);
+  console.log("Temperature: " + newTemp);
+  // now get the humidity from the data and set that to a variable
+  var humidity = response.main.humidity;
+  console.log("humidity: " + humidity);
+ // we get the windspeed value from the data and set it to a value 
+  var windSpeed = response.wind.speed;
+  console.log("windspeed: " + windSpeed);
 
-
-// for(var i = 0; response.data.length; i++){
-//   // var imageEl = $("<img>");
-//   // $("body").append(imageEl);
-//   // imageEl.attr("src", response.data[i].images.original.url);
-// }
- console.log(response);
-
-  // imageEl.text(response.)
 });
  });
  
