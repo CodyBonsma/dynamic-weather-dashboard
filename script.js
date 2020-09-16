ebed5726fca8a8ff0549437d65794456bbff213f
Cody Bonsma <codybonsma@gmail.com>
2020-09-15 23:02:35 -0400

    added the weather icons to the forecast and main city div. added some styling to the main city title as well

--- script.js
+++ script.js
@@ -6,13 +6,10 @@ $(document).ready(function () {
   var searchBtn = $("#search-btn");
   var mainForecastEl = $("#main-forecast");
   var subForecastEl = $("#five-day-forecast");
-
   var singleTruth = [];
 
   // on click, we want to dynamically populate the main forecast and five day forecast
-  
   searchBtn.on("click", function () {
-    
     // save user input to localStorage and populate new button of previous searches
     singleTruth.push(searchInputEl.val());
     localStorage.setItem("city", JSON.stringify(singleTruth));
@@ -35,17 +32,20 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log(response.main.temp);
+      // console.log(response.main.temp);
       // get the Kelvin temperature from the api and set it to Fahrenheit
       var kelvinTemp = response.main.temp;
       var newTemp = Math.round(((kelvinTemp - 273.15) * 9) / 5 + 32);
-      console.log("Temperature: " + newTemp);
+      // console.log("Temperature: " + newTemp);
       // now get the humidity from the data and set that to a variable
       var humidity = response.main.humidity;
-      console.log("humidity: " + humidity);
+      // console.log("humidity: " + humidity);
       // we get the windspeed value from the data and set it to a value
       var windSpeed = response.wind.speed;
-      console.log("windspeed: " + windSpeed);
+      // console.log("windspeed: " + windSpeed);
+      //get the current icon from the url
+      var todayIcon = response.weather[0].icon;
+      console.log(todayIcon);
 
       var currentMoment = moment().format("DD/MM/YYYY");
       // now we populate the main forecast El with these values
@@ -54,7 +54,14 @@ $(document).ready(function () {
       // create the main city title for the element
       var todayCity = $("<h3>");
       todayCity.text(searchInputEl.val() + "  " + currentMoment);
+      todayCity.addClass("city-style");
       mainWeatherDiv.append(todayCity);
+      // append today's weather icon
+      var todayIconImg = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" + todayIcon + "@2x.png"
+      );
+      mainWeatherDiv.append(todayIconImg);
       // today's temperature
       var todayTemp = $("<h5>");
       todayTemp.text("Temperature: " + newTemp + "F");
@@ -84,23 +91,33 @@ $(document).ready(function () {
         // retrieve the 5 day forecast weather data
 
         // get the forecast kelvinTemp and calculate F
-        var subKelvinTemp = result.list[i].main.temp;
+        var subKelvinTemp = result.list[i * 8 - 1].main.temp;
         var subNewTemp = Math.round(((subKelvinTemp - 273.15) * 9) / 5 + 32);
-        console.log("Temperature: " + subNewTemp);
+        // console.log("Temperature: " + subNewTemp);
         //grab the humidity level for the 5 day forecast
-        var subHumidity = result.list[i].main.humidity;
-        console.log("humidity level: " + subHumidity + "%");
+        var subHumidity = result.list[i * 8 - 1].main.humidity;
+        // console.log("humidity level: " + subHumidity + "%");
         // generate the next 5 dates to be logged in the forecast
         var futureDays = moment().add(i, "days").format("DD/MM/YYYY");
-        console.log(futureDays);
+        // console.log(futureDays);
+        //get the current icon from the url
+        var dayIcon = result.list[i * 8 - 1].weather[0].icon;
+        console.log(dayIcon);
+
         //dynamically create the 5 day forecast div and include the new values
         var emptyEl = $("<div>");
-        emptyEl.addClass("test forecast-box");
+        emptyEl.addClass("forecast-box");
         subForecastEl.append(emptyEl);
         // dynamically create a title that has next day's date
         var nextDay = $("<h5>");
         nextDay.text(futureDays);
         emptyEl.append(nextDay);
+        // grab the icon and appending it to the empty El
+        var iconImg = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/wn/" + dayIcon + "@2x.png"
+        );
+        emptyEl.append(iconImg);
         // dynamically create the temperature forecast
         var titleTemp = $("<h6>");
         titleTemp.text("Temperature: " + subNewTemp + "F");
